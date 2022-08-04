4f5d4f38df3529e46537969904ae82cb5e1955cd
Hanimi Reddy <hreddy@delaplex.com>
2022-08-04 18:10:31 +0530

    CHC - Config - Smoke - User amnagement

---
+++ ProductLibrary/CHC_Config/TestPlan/User_Management/CHC_TP_422702_Users_SmokeTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHC_Config.TestPlan.User_Management
+{
+    class CHC_Config_Users_Smoketest
+    {
+    }
+}
